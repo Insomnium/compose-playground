7d9e6d3f116fd4ea25c8da0194c54bd8cc3eaddb
ins137 <ins137@gmail.com>
2025-01-13 16:23:25 +0400

    Add canvas playground

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,40 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Canvas
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.isSecondaryPressed
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlin.math.roundToInt
 
 @Composable
 @Preview
@@ -30,24 +48,28 @@ fun App() {
     }
 
     MaterialTheme {
-        Row(modifier = modifier) {
-            Column {
-                Button(onClick = {
-                    count.value++
-                    if (count.value % 2 == 0) {
+        var offsetX by remember { mutableStateOf(0f) }
+        var offsetY by remember { mutableStateOf(0f) }
 
+        Surface {
+            Box(Modifier.fillMaxSize()) {
+                Canvas(Modifier
+                    .size(60.dp)
+                    .pointerInput(Unit) {
+                        detectDragGestures { change, dragOffset ->
+                            offsetX += dragOffset.x
+                            offsetY += dragOffset.y
+                        }
                     }
-                }) {
-                    Text(if (count.value == 0) "Press me" else "Hello, ${count.value}")
-                }
-                Button(onClick = { count.value = 0 }) {
-                    Text("Reset counter")
-                }
-            }
-            LazyColumn {
-                items(count = count.value) {
-                    Text("Item: $it")
-                }
+                    .offset { IntOffset(x = offsetX.roundToInt(), y = offsetY.roundToInt()) }
+                    .padding(8.dp)
+                    .size(100.dp)
+                    .border(1.dp, Color.Black)
+                    .background(Color.Cyan)
+                    .drawBehind {
+                        drawCircle(Color.Red, size.minDimension / 3)
+                    }
+                ) {}
             }
         }
     }
